192bbb33f5ae1f748132846f63bf3fa5b7c17c59
SpencerLaw <spencerlaw@example.com>
2025-12-04 09:31:59 +0800

    修复粒子效果中形状和颜色选择按钮无法点击的问题

--- components/ThreeJSParticles.tsx
+++ components/ThreeJSParticles.tsx
@@ -441,7 +441,7 @@ const ThreeJSParticles: React.FC = () => {
       />
 
       {/* Three.js Container */}
-      <div ref={containerRef} style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 2 }} />
+      <div ref={containerRef} style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 2, pointerEvents: 'none' }} />
 
       {/* Main UI Container */}
       <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', pointerEvents: 'none', zIndex: 10 }}>

10d38ad6dec72847b46bb4af8a98da7df0922201
SpencerLaw <spencerlaw@example.com>
2026-03-03 10:00:28 +0800

    Fix: Change PWA shortcut icons to PNG for better compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -31,35 +31,35 @@ export default defineConfig(({ mode }) => {
               short_name: '广播',
               description: '打开广播接收助手',
               url: '/broadcast',
-              icons: [{ src: '/icons/icon-192x192.svg', sizes: '192x192' }]
+              icons: [{ src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' }]
             },
             {
               name: '分贝仪',
               short_name: '分贝',
               description: '哆啦A梦分贝监测',
               url: '/doraemon',
-              icons: [{ src: '/icons/icon-192x192.svg', sizes: '192x192' }]
+              icons: [{ src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' }]
             },
             {
               name: '奖惩大冒险',
               short_name: '大冒险',
               description: '趣味奖惩游戏',
               url: '/adventure',
-              icons: [{ src: '/icons/icon-192x192.svg', sizes: '192x192' }]
+              icons: [{ src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' }]
             },
             {
               name: '随机分组',
               short_name: '分组',
               description: '快速学生分组工具',
               url: '/group-maker',
-              icons: [{ src: '/icons/icon-192x192.svg', sizes: '192x192' }]
+              icons: [{ src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' }]
             },
             {
               name: '晨光能量树',
               short_name: '能量树',
               description: '晨起打卡能量收集',
               url: '/kiddie-plan',
-              icons: [{ src: '/icons/icon-192x192.svg', sizes: '192x192' }]
+              icons: [{ src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' }]
             }
           ],
           icons: [
